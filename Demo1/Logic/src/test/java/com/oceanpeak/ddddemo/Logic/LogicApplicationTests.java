a79eb5f742b9aad85ff658df75e2c00b1ac332ca
James Liu <jamesliu_8@yahoo.com>
2020-01-08 15:15:48 -0500

    This is first time commit

---
+++ Demo1/Logic/src/test/java/com/oceanpeak/ddddemo/Logic/LogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceanpeak.ddddemo.Logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
